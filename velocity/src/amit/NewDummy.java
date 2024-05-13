bc098c50617b360000ebd61092200e337ba7a8cf
Admin <Admin@Amit>
2024-05-13 13:05:53 +0530

    Dummy - Delete New Dummy - Create

---
+++ velocity/src/amit/NewDummy.java
@@ -0,0 +1,32 @@
+package amit;
+
+import java.util.Random;
+
+public class NewDummy {
+	
+	public static void main(String[] args) {
+		
+
+		
+		
+		{
+			Random rn = new Random();
+		    int value = rn.nextInt(86);
+		    System.out.print(value + ")");
+		    
+		    
+			switch(value)
+			{
+			
+			    case 0 : System.out.println("Tell me about your self.");
+			    break;
+			    
+			    default:System.out.println("No Question");
+				break;
+		
+				
+				
+	         }
+		}
+}
+}
