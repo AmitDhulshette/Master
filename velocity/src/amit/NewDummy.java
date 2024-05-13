ac6f19cc661affbb8a864fe078b2929bc5b0cae9
Admin <Admin@Amit>
2024-05-13 21:46:01 +0530

    again

--- velocity/src/amit/NewDummy.java
+++ velocity/src/amit/NewDummy.java
@@ -22,14 +22,7 @@ public class NewDummy {
 			    break;
 			    
 
-			    case 1 : System.out.println("dhfsvsadfbvsdvsd");
-			    break;
-			    
-			    case 2 : System.out.println("jsfdkawfd.");
-			    break;
-			    
-			    case 3 : System.out.println("jsfdgkawfd.");
-			    break;
+			   
 			    
 			    case 4 : System.out.println("jsfdkawfd.");
 			    break;
