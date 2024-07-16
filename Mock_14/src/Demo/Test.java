1fe083c9aeed4f617fca19b5ed93c76ff2961104
AmitDhulshette <dhulshetteamit@gmail.com>
2024-07-16 21:19:10 +0530

    Update Test.java

--- Mock_14/src/Demo/Test.java
+++ Mock_14/src/Demo/Test.java
@@ -3,10 +3,6 @@ package Demo;
 public class Test {
 
 	public static void main(String[] args) {
-		
-	System.out.println("Hi");
-	
-	System.out.println("Hello");
 
 	System.out.println("Amit");	
 		
